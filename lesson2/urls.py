b44857e7146b0a3a61bea279898faa44dfc33350
RomanG-sky <89301585+RomanG-sky@users.noreply.github.com>
2022-09-10 00:52:31 +0300

    task2

--- lesson2/urls.py
+++ lesson2/urls.py
@@ -27,20 +27,20 @@ def test_main(request):
     return HttpResponse(f'<h1>{test_main.__name__}</h1>')
 
 
-def main_page_home_python(request):
-    return HttpResponse(f'<h1>{main_page_home_python.__name__}</h1>')
-
-def path_link(link):
-    return f'{link}/'
-def link(request):
-    return HttpResponse(f'<h1><a href>127.0.0.1:8000/task2.3/</a><h1>')
+# def main_page_home_python(request):
+#     return HttpResponse(f'<h1>{main_page_home_python.__name__}</h1>')
+#
+# def path_link(link):
+#     return f'{link}/'
+# def link(request):
+#     return HttpResponse(f'<h1><a href>127.0.0.1:8000/task2.3/</a><h1>')
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test/', test),
-    path('test/main/', test_main),
-    path('main_page/home/python/', main_page_home_python),
-    path(path_link('task2.3'),link),
+    # path('test/', test),
+    # path('test/main/', test_main),
+    # path('main_page/home/python/', main_page_home_python),
+    # path(path_link('task2.3'),link),
 ]
